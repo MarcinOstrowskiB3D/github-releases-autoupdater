832a62042bc5309acac3b43311f463f202c9a563
Marcin Ostrowski <pmostrowski96@gmail.com>
2023-08-25 15:46:22 +0200

    github-releases-autoupdater: added polish translation.

---
+++ cupdaterdialog_pl.ts
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>CUpdaterDialog</name>
+    <message>
+        <location filename="src/updaterUI/cupdaterdialog.ui" line="17"/>
+        <source>Update checker</source>
+        <translation>Sprawdzanie aktualizacji</translation>
+    </message>
+    <message>
+        <location filename="src/updaterUI/cupdaterdialog.ui" line="35"/>
+        <source>Searching for updates...</source>
+        <translation>Szukanie aktualizacji...</translation>
+    </message>
+    <message>
+        <location filename="src/updaterUI/cupdaterdialog.ui" line="90"/>
+        <source>A new version is available!</source>
+        <translation>Dostępna jest nowa wersja!</translation>
+    </message>
+    <message>
+        <location filename="src/updaterUI/cupdaterdialog.cpp" line="23"/>
+        <source>Install</source>
+        <translation>Zainstaluj</translation>
+    </message>
+    <message>
+        <location filename="src/updaterUI/cupdaterdialog.cpp" line="47"/>
+        <source>Downloading the update...</source>
+        <translation>Pobieranie aktualizacji...</translation>
+    </message>
+    <message>
+        <location filename="src/updaterUI/cupdaterdialog.cpp" line="58"/>
+        <source>Manual update required</source>
+        <translation>Wymagana ręczna aktualizacja</translation>
+    </message>
+    <message>
+        <location filename="src/updaterUI/cupdaterdialog.cpp" line="59"/>
+        <source>Automatic update is not supported on this operating system. Do you want to download and install the update manually?</source>
+        <translation>Automatyczna aktualizacja nie jest wspierana dla tego systemu operacyjnego. Czy chcesz pobrać i zainstalować aktualizację ręcznie?</translation>
+    </message>
+    <message>
+        <location filename="src/updaterUI/cupdaterdialog.cpp" line="84"/>
+        <source>No update available</source>
+        <translation>Brak dostępnych aktualizacji</translation>
+    </message>
+    <message>
+        <location filename="src/updaterUI/cupdaterdialog.cpp" line="84"/>
+        <source>You already have the latest version of the program.</source>
+        <translation>Program jest zaktualizowany do najnowszej wersji.</translation>
+    </message>
+    <message>
+        <location filename="src/updaterUI/cupdaterdialog.cpp" line="104"/>
+        <source>Error checking for updates</source>
+        <translation>Sprawdzanie błędów aktualizacji</translation>
+    </message>
+</context>
+</TS>
