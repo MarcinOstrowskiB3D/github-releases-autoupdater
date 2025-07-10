750227026ed3ea817c3ade3438987bccb1f960bf
pietr <pg@b3d.com.pl>
2025-07-10 12:05:47 +0200

    GitAutoUpdated: added german translation (beta)

---
+++ cupdaterdialog_de.ts
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>CUpdaterDialog</name>
+    <message>
+        <location filename="src/updaterUI/cupdaterdialog.ui" line="17"/>
+        <source>Update checker</source>
+        <translation type="unfinished">Akutualisierung überprüfen</translation>
+    </message>
+    <message>
+        <location filename="src/updaterUI/cupdaterdialog.ui" line="35"/>
+        <source>Searching for updates...</source>
+        <translation type="unfinished">Aktualisierung suchen...</translation>
+    </message>
+    <message>
+        <location filename="src/updaterUI/cupdaterdialog.ui" line="90"/>
+        <source>A new version is available!</source>
+        <translation type="unfinished">Neue Version ist zugänglich</translation>
+    </message>
+    <message>
+        <location filename="src/updaterUI/cupdaterdialog.cpp" line="33"/>
+        <source>Install</source>
+        <translation type="unfinished">Installlieren</translation>
+    </message>
+    <message>
+        <location filename="src/updaterUI/cupdaterdialog.cpp" line="76"/>
+        <source>Downloading the update...</source>
+        <translation type="unfinished">Aktualisierung downladen</translation>
+    </message>
+    <message>
+        <location filename="src/updaterUI/cupdaterdialog.cpp" line="88"/>
+        <source>Manual update required</source>
+        <translation type="unfinished">manuelle Aktualisierung erforderlich</translation>
+    </message>
+    <message>
+        <location filename="src/updaterUI/cupdaterdialog.cpp" line="89"/>
+        <source>Automatic update is not supported on this operating system. Do you want to download and install the update manually?</source>
+        <translation type="unfinished">Für dieses Betriebssystem wird keine automatische Aktualisierung unterstützt. Möchten Sie die Aktualisierung manuell herunterladen und installieren?</translation>
+    </message>
+    <message>
+        <location filename="src/updaterUI/cupdaterdialog.cpp" line="126"/>
+        <source>No update available</source>
+        <translation type="unfinished">keine zugängliche Aktualisierung</translation>
+    </message>
+    <message>
+        <location filename="src/updaterUI/cupdaterdialog.cpp" line="126"/>
+        <source>You already have the latest version of the program.</source>
+        <translation type="unfinished">Das Programm wurde auf die neueste Version aktualisiert.</translation>
+    </message>
+    <message>
+        <location filename="src/updaterUI/cupdaterdialog.cpp" line="149"/>
+        <source>Error checking for updates</source>
+        <translation type="unfinished">Überprüfung von Aktualisierungsfehlern</translation>
+    </message>
+</context>
+</TS>
